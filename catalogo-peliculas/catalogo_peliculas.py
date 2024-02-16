a5082b3ab7205af6dcebc6afc350a3f465670c03
Victor <VictorCarmona9035@hotmail.com>
2024-02-16 22:09:16 +0100

    Frame en Tkinter

--- catalogo-peliculas/catalogo_peliculas.py
+++ catalogo-peliculas/catalogo_peliculas.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from client.gui_app import Frame
 
 
 def main():
@@ -6,9 +7,16 @@ def main():
     ##Agregamos un titulo a la ventana
     root.title('Catalogo de Peliculas')
     root.iconbitmap('img/pelicula.ico')
-    root.resizable(1,0)
+    root.resizable(0,0)
 
-    root.mainloop()
+
+    app = Frame(root = root)
+    ##frame =tk.Frame(root)
+    ##frame.pack()
+    ##frame.config(width=480, height=320, bg='green')'''
+    #root.mainloop()
+
+    app.mainloop()
 
 if __name__ =='__main__':
     main()
